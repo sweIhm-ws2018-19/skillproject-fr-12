63a0d0d6d1ef60cc8fa1ceef5a6bc6d442758349
npkllr <33007038+npkllr@users.noreply.github.com>
2018-12-10 15:32:08 +0100

    parse Json testen

--- src/main/java/soupit/hilfsklassen/JsonService.java
+++ src/main/java/soupit/hilfsklassen/JsonService.java
@@ -1,8 +1,14 @@
 package soupit.hilfsklassen;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import soupit.model.Rezept;
 import soupit.model.Zutat;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -24,10 +30,34 @@ public class JsonService {
         ArrayList<Rezept> rezepte = new ArrayList<>();
 
         //hier Json einlesen
-        ArrayList<Integer> zutaten = new ArrayList<>(Arrays.asList(new Integer[]{0, 1, 2}));
-        ArrayList<Integer> mengen = new ArrayList<>(Arrays.asList(new Integer[]{1, 14, 10}));
-        rezepte.add(new Rezept(0, "kartoffelsuppe", null, zutaten, mengen));
-        rezepte.add(new Rezept(1, "karottensuppe", null, zutaten, mengen));
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader("data/rezepte.json");
+
+            BufferedReader br = new BufferedReader(fileReader);
+
+            String input = "";
+
+            String zeile = br.readLine();
+
+            while (zeile != null) {
+                input += zeile;
+                zeile = br.readLine();
+            }
+
+            JSONArray json = new JSONObject(input).getJSONArray("rezepte");
+
+            for(Object j : json){
+                rezepte.add(rezeptParsen((JSONObject) j));
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File nicht vorhanden..");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Irgendein Fehler in JsonService.rezepteEinlesen()..");
+            e.printStackTrace();
+        }
 
         return rezepte;
     }
@@ -44,5 +74,32 @@ public class JsonService {
                 "prisen", "w"));
 
         return zutaten;
+    }
+
+    private static Rezept rezeptParsen(JSONObject j) {
+        int newRezeptID = (int) j.get("rezeptID");
+        String newName = (String) j.get("name");
+        ArrayList<String> newSchritte = new ArrayList<>();
+        ArrayList<Integer> newZutaten = new ArrayList<>();
+        ArrayList<Integer> newMengen = new ArrayList<>();
+
+        JSONArray jsonSchritte = j.getJSONArray("schritte");
+        for (Object schritt : jsonSchritte) {
+            newSchritte.add((String) schritt);
+        }
+
+        JSONArray jsonZutaten = j.getJSONArray("zutaten");
+        for (Object zutat : jsonZutaten) {
+            newZutaten.add((Integer) zutat);
+        }
+
+        JSONArray jsonMengen = j.getJSONArray("mengen");
+        for (Object menge : jsonMengen) {
+            newMengen.add((Integer) menge);
+        }
+
+        Rezept r2 = new Rezept(newRezeptID, newName, newSchritte, newZutaten, newMengen);
+
+        return r2;
     }
 }
--- src/main/java/soupit/hilfsklassen/NurEinKleinerTest.java
+++ src/main/java/soupit/hilfsklassen/NurEinKleinerTest.java
@@ -5,16 +5,18 @@ import org.json.JSONObject;
 import soupit.model.Rezept;
 import soupit.model.ZutatMenge;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class NurEinKleinerTest {
 
     public static void main(String... args){
-        //System.out.println(TextService.zutatenVonRezeptVorlesen(RezeptService.getZutaten(0)));
+        System.out.println(TextService.zutatenVonRezeptVorlesen(RezeptService.getZutaten(0)));
 
-        testnochwas();
+        //testirgendwas();
     }
 
     public static void testirgendwas(){
@@ -60,5 +62,54 @@ public class NurEinKleinerTest {
         Rezept r2 = new Rezept(newRezeptID, newName, newSchritte, newZutaten, newMengen);
 
         System.out.println(r2.getMengen());
+    }
+
+    public static void testnochmalwas() throws IOException {
+        //hier Json einlesen
+        FileReader fileReader = new FileReader("data/rezepte.json");
+
+        BufferedReader br = new BufferedReader(fileReader);
+
+        String input = "";
+
+        String zeile = br.readLine();
+
+        while( zeile != null )
+        {
+            input += zeile;
+            zeile = br.readLine();
+        }
+
+        JSONObject json = new JSONObject(input);
+        JSONObject j = (JSONObject) json.get("1");
+
+        //System.out.println(j);
+
+
+        int newRezeptID = (int)j.get("rezeptID");
+        String newName =  (String)j.get("name");
+        ArrayList<String> newSchritte = new ArrayList<>();
+        ArrayList<Integer> newZutaten = new ArrayList<>();
+        ArrayList<Integer> newMengen = new ArrayList<>();
+
+        JSONArray jsonSchritte = j.getJSONArray("schritte");
+        for(Object schritt: jsonSchritte){
+            newSchritte.add((String) schritt);
+        }
+
+        JSONArray jsonZutaten = j.getJSONArray("zutaten");
+        for(Object zutat: jsonZutaten){
+            newZutaten.add((Integer) zutat);
+        }
+
+        JSONArray jsonMengen = j.getJSONArray("mengen");
+        for(Object menge: jsonMengen){
+            newMengen.add((Integer) menge);
+        }
+
+        Rezept r2 = new Rezept(newRezeptID, newName, newSchritte, newZutaten, newMengen);
+
+        System.out.println(r2.getMengen());
+
     }
 }
