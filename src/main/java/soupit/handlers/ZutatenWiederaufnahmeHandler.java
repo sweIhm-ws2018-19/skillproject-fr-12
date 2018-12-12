b5ee128fb18b403e70daa2ba75c369274d96c1d7
HauslerSebastian <sw.hausler@t-online.de>
2018-12-12 23:34:18 +0100

    ZutatenWiederaufnahmeHandler base Structure

--- src/main/java/soupit/handlers/ZutatenWiederaufnahmeHandler.java
+++ src/main/java/soupit/handlers/ZutatenWiederaufnahmeHandler.java
@@ -2,18 +2,51 @@ package soupit.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.Response;
+import com.amazon.ask.model.*;
+import com.amazon.ask.response.ResponseBuilder;
+import soupit.hilfsklassen.SlotFilter;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
 
 public class ZutatenWiederaufnahmeHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput input) {
-        return false;
+        return input.matches(intentName("ZutatenAuswahlIntent"));
     }
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        return Optional.empty();
+        Request request = input.getRequestEnvelope().getRequest();
+        IntentRequest intentRequest = (IntentRequest) request;
+        Intent intent = intentRequest.getIntent();
+        //get slots from current intend
+        Map<String, Slot> slots = intent.getSlots();
+
+        final ArrayList<String> zutatStringList = (ArrayList<String>) SlotFilter.getIngredient(slots);
+
+        final String speechText;
+        final String repromptText;
+        boolean isAskResponse = false;
+
+        speechText = "";
+        repromptText = "";
+
+
+        ResponseBuilder responseBuilder = input.getResponseBuilder();
+
+        responseBuilder.withSimpleCard("SoupitSession", speechText)
+                .withSpeech(speechText)
+                .withShouldEndSession(false);
+
+        if (isAskResponse) {
+            responseBuilder.withShouldEndSession(false)
+                    .withReprompt(repromptText);
+        }
+
+        return responseBuilder.build();
     }
 }
