e825d8866e68c329d02b52c5fe9527411a2cfdc5
npkllr <33007038+npkllr@users.noreply.github.com>
2018-12-17 22:29:12 +0100

    unit tests added (TextService)

--- src/test/java/soupit/hilfsklassen/TextServiceTest.java
+++ src/test/java/soupit/hilfsklassen/TextServiceTest.java
@@ -3,6 +3,9 @@ package soupit.hilfsklassen;
 import org.junit.Test;
 import soupit.handlers.TestHelper;
 import soupit.model.Rezept;
+import soupit.model.ZutatMenge;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -20,7 +23,17 @@ public class TextServiceTest {
 
     @Test
     public void testZutatenVonRezeptVorlesen(){
+        ZutatMenge zm = new ZutatMenge("zehn", "gramm", "kartoffel");
+        ArrayList<ZutatMenge> list = new ArrayList<>();
+        list.add(zm);
+        list.add(zm);
+        list.add(zm);
 
+        String have = TextService.zutatenVonRezeptVorlesen(list, 2);
+
+        assertNotNull(have);
+        assertTrue(have.contains("Für die Zubereitung"));
+        assertTrue(have.contains("Hast du alle Zutaten vorrätig?"));
     }
 
     @Test
