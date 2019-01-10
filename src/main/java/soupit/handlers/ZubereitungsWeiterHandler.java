8715666c7b8ce18453427344dc672fc38aef60f8
npkllr <33007038+npkllr@users.noreply.github.com>
2019-01-10 23:53:23 +0100

    code coverage + duplication

--- src/main/java/soupit/handlers/ZubereitungsWeiterHandler.java
+++ src/main/java/soupit/handlers/ZubereitungsWeiterHandler.java
@@ -3,23 +3,14 @@ package soupit.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import soupit.hilfsklassen.*;
-import soupit.model.Rezept;
-import soupit.model.RezeptCount;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
 
 public class ZubereitungsWeiterHandler implements RequestHandler {
-    private final static String PORTIONEN_YES_INTENT = "PortionenYesIntent";
-    private final static String REZEPT_FOUND = "REZEPT_FOUND";
-    private final static String REZEPT_INDEX = "REZEPT_INDEX";
-    private final static String CURRENT_REZEPT = "CURRENT_REZEPT";
 
 
     public boolean canHandle(HandlerInput input) {
@@ -34,10 +25,8 @@ public class ZubereitungsWeiterHandler implements RequestHandler {
         //get slots from current intent s
         Map<String, Slot> slots = intent.getSlots();
 
-        String speechText = ZubereitungsSteuerungsLogik.getNext(input, slots, 1);;
+        String speechText = ZubereitungsSteuerungsLogik.getNext(input, slots, 1);
 
-
-        //SessionAttributeService.updateLastIntent(input, "ZubereitungsWeiterHandler");
         return input.getResponseBuilder()
                 .withSimpleCard("SoupitSession", speechText)
                 .withSpeech(speechText)
---
+++ src/test/java/soupit/handlers/ZubereitungsWeiterHandlerTest.java
@@ -0,0 +1,42 @@
+package soupit.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.ui.SsmlOutputSpeech;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+public class ZubereitungsWeiterHandlerTest {
+    private ZubereitungsWeiterHandler handler;
+
+    @Before
+    public void setup(){
+        handler = new ZubereitungsWeiterHandler();
+    }
+
+    @Test
+    public void testCanHandle() {
+        HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(handler.canHandle(inputMock));
+    }
+
+    @Test
+    public void testHandle() {
+        HandlerInput input = TestHelper.mockInputWithEmptySlot("CURRENT_REZEPT");
+
+        Response response = handler.handle(input).get();
+        SsmlOutputSpeech ssmlOut = (SsmlOutputSpeech) response.getOutputSpeech();
+        String have = ssmlOut.getSsml();
+
+        assertFalse(response.getShouldEndSession());
+        assertNotNull(have);
+    }
+}
